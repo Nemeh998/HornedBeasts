d235c6002506c9cb6b538a944096a206dbf5f212
nemh <nemh998@gmail.com>
2021-06-08 17:09:54 +0300

    lab2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HEDAER from './component/HEADER';
 import Main from './component/main';
 import Footer from './component/Footer';
+
 class App extends React.Component {
 
   render() {
--- src/component/HEADER.js
+++ src/component/HEADER.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import HornedBeast from './component/HornedBeast';  
 
 class HEDAER extends React.Component{
