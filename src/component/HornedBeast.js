0a20c3ca85c52141c6b0f594ab08bdcf0ed655e2
nemh <nemh998@gmail.com>
2021-06-12 12:20:01 +0300

    modal

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -2,7 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import SelectedBeast from  './component/SelectedBeast'
+import Modal from 'react-bootstrap/Modal'
+// import SelectedBeast from  './component/SelectedBeast'
 
   class HornedBeast extends React.Component{
     
@@ -46,7 +47,7 @@ render() {
    
   
 <div>
-<Card style={{ width: '18rem' }}>
+<Card style={{ width: '18rem' }} onClick={this.openModal}>
 <Card.Title>{this.props.title}</Card.Title>
 <Card.Img onClick={this.increaseNumberOfPets}variant="top" src={this.props.img} alt={this.props.alt} title={this.props.title} />
 <Card.Text>{this.props.description}</Card.Text>
@@ -54,21 +55,38 @@ render() {
     <Card.Text>
     ❤ : {this.state.numberclick}
             </Card.Text>
-            <Card.Text>
-            😸 Number of Taken Tuna: {this.state.clickonbutton}
-            </Card.Text>
-    <Button onClick={this.needTuna} variant="primary">Go somewhere</Button>
-    <Button onClick={this.openModal} >Click here to see as a modal</Button>
+            {/* <Card.Text>
+          : {this.state.clickonbutton}
+            </Card.Text> */}
+   
+    {/* <Button  >Click here to see as a modal</Button> */}
 
 </Card>
-      <SelectedBeast
+      {/* <SelectedBeast
           activeModal={this.state.modalState}
           closeModal={this.closeModal}
           title={this.props.title}
           src={this.props.imageUrl}
           description={this.props.description}
           keyword={this.props.keyword}
-        />
+        /> */}
+
+<Modal show={this.state.modalState}  alt={this.props.title} onHide={this.closeModal}>
+          <Modal.Title>{this.props.title}</Modal.Title>
+          <Modal.Body> 
+            <img src={this.props.img} alt={this.props.keyword} title={this.props.title} style={{ width: '100%' }} />
+            <p>{this.props.description}</p>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.closeModal}>
+              Close
+          </Button>
+         
+          </Modal.Footer>
+        </Modal>
+
+
     </div>
 
 
